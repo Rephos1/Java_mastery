a177df8e4d66ca22c8adced8da18ec32af4bfc43
Mihaly Arpad Dani <dmisi98@gmail.com>
2024-04-23 14:25:26 +0100

    new push

---
+++ test/cwk4/TournamentTest.java
@@ -0,0 +1,72 @@
+package cwk4;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert.*;
+
+class TournamentTest {
+
+    @org.junit.jupiter.api.Test
+    void getMoney() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReserve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getChampionDetails() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isInReserve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enterChampion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isInViziersTeam() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void retireChampion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTeam() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDisqualified() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChallenge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getChallenge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllChallenges() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void meetChallenge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readChallenges() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadGame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveGame() {
+    }
+}
